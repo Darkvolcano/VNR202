73668a0a04f63059e800c5a258e59dbb41465828
Darkvolcano <khangphamchuchi@gmail.com>
2025-11-05 17:27:39 +0700

    add information team

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AIChat from "./pages/AIChat";
-import TuTuong from "./pages/TuTuong";
+import Ideology from "./pages/Ideology";
 import Information from "./pages/Information";
 
 function App() {
@@ -17,12 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/ai-chat" element={<AIChat />} />
-          <Route path="/tu-tuong" element={<TuTuong />} />
-          <Route path="/thong-tin" element={<Information />} />
+          <Route path="/ideology" element={<Ideology />} />
+          <Route path="/information" element={<Information />} />
         </Routes>
       </main>
 
-      {/* Footer cũng sẽ luôn hiển thị */}
       <Footer />
     </div>
   );
